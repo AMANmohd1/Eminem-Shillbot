2277aecb9dc0f575e618facfe5e1f39907e6da83
AMAN <88148056+AMANmohd1@users.noreply.github.com>
2021-10-28 19:08:30 +0000

    Connected youtube API, function to fetch comments from "RAP GOD" video

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import discord
 import os
+import requests
+import json
+from googleapiclient.discovery import build
+from bs4 import BeautifulSoup
+from random import randint
 
 client = discord.Client()
+my_secret2 = os.environ['You_API']
 my_secret = os.environ['TOKEN']
+video_id = "XbGs_qK2PQA" #rap god
+
+youtube = build('youtube','v3',developerKey=my_secret2)
+
+request = youtube.commentThreads().list(
+  part="snippet",
+  videoId=video_id,
+  order="relevance"
+)
+
+def get_com():
+  response = request.execute()
+  items = response["items"][randint(0,19)]
+  item_info = items["snippet"]
+  topLevelComment = item_info["topLevelComment"]
+  comment_info = topLevelComment["snippet"]
+  return(comment_info["textDisplay"])
+  # for item in items:
+  #  item_info = item["snippet"]
+  #  topLevelComment = item_info["topLevelComment"]
+  #  comment_info = topLevelComment["snippet"]
+  #  return(comment_info["textDisplay"]) 
 
 
 @client.event
@@ -16,5 +44,11 @@ async def on_message(msg):
 
   if msg.content.startswith('!eminem'):
     await msg.channel.send('MOST UNDERRATED RAPPER')
+
+  if msg.content.startswith('!comment'):
+    comment = get_com()
+    gfg = BeautifulSoup(comment,features="html5lib")
+    res = gfg.get_text('\n')
+    await msg.channel.send(res)
 
 client.run(my_secret)
